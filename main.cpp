0fc95c2854328fb182c49f6de7a8ba0d3c4f73c7
Ryan <ryan@projecthax.com>
2013-11-02 14:04:40 -0400

    Code improvements

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <string>
 
-#include <boost/regex.hpp>
+#ifndef NO_BOOST
+	#include <boost/regex.hpp>
+#else
+	#error "Boost.Regex is required for SilkroadLinux to compile successfully"
+	#error "https://github.com/ProjectHax/SilkroadLinux"
+#endif
 
 void PatchEXE(const std::string & path)
 {
 	std::ifstream ifs(path.c_str(), std::ios::in | std::ios::binary);
 	if(ifs.is_open())
 	{
-		ifs.seekg(0, std::ios::end);
-		uint64_t size = ifs.tellg();
-		ifs.seekg(0, std::ios::beg);
-
-		std::string data;
-		data.resize(static_cast<uint32_t>(size));
-		ifs.read(&data[0], size);
+		std::string data((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
 		ifs.close();
 
 		const boost::regex e("\\x83\\xF8\\xFF(?<data>\\x75\\x19)\\x68....\\x55\\xFF\\x15....\\x3B\\xC3\\xA3....\\x0F\\x84....\\x3B\\xEB\\x74\\x07\\x55\\xFF\\x15....\\x56\\xFF\\x15....\\x8B\\x4C\\x24\\x44\\x5F\\x5E\\x5D\\x5B\\x33\\xCC\\xB8\\x01\\x00\\x00\\x00\\xE8....\\x83\\xC4\\x3C\\xC3\\xCC");
@@ -34,15 +33,15 @@ void PatchEXE(const std::string & path)
 		}
 
 		const char patch = 0xEB;
-		int32_t count = 0;
+		int count = 0;
 
 		try
 		{
 			while(boost::regex_search(start, end, what, e, flags))
 			{
-				uint32_t offset = what.position("data");
+				size_t offset = what.position("data");
 
-				std::cout << "\t" << "patch offset: " << (int64_t)offset << std::endl;
+				std::cout << "\t" << "patch offset: " << offset << std::endl;
 
 				fs.seekg(offset, std::ios::beg);
 				fs.write(&patch, 1);
@@ -76,7 +75,7 @@ void PatchEXE(const std::string & path)
 	}
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::cout << "ProjectHax Silkroad For Linux Patcher" << std::endl << std::endl;
 
